2f3f40fa9e440e718fc7e702d1ea2cb0d044acb3
Chikka Nikhil Eswar Anand <129603787+anandchikka@users.noreply.github.com>
2024-03-15 15:07:01 +0530

    Create BeautifulMatrix.java

---
+++ Day1/BeautifulMatrix.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class BeautifulMatrix
+{
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i1=0;
+        int j1=0;
+        int[][] arr=new int[5][5];
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                arr[i][j]=sc.nextInt();
+                if(arr[i][j]==1){
+                    i1=i;
+                    j1=j;
+                }
+            }
+        }
+        int count=0;
+        if(i1>=2 && j1>=2){
+            count=(i1-2)+(j1-2);
+        }
+        else if(i1>=2 && j1<=2){
+            count=(i1-2)+(2-j1);
+        }
+        else if(i1<=2 && j1>=2){
+            count=(2-i1)+(j1-2);
+        }
+        else if(i1<=2 && j1<=2){
+            count=(2-i1)+(2-j1);
+        }
+        
+        System.out.print(count);
+	}
+}
