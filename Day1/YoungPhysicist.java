0e49e09419b3176a5de39b758ef3b604c5801ccb
Chikka Nikhil Eswar Anand <129603787+anandchikka@users.noreply.github.com>
2024-03-15 15:09:25 +0530

    Update YoungPhysicist.java

--- Day1/YoungPhysicist.java
+++ Day1/YoungPhysicist.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
-public class YoungPhysicist
-{
+public class YoungPhysicist{
 	public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
