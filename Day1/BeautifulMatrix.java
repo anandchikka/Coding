5eeffde3c978444269d4cf01cc0ae68381200634
Chikka Nikhil Eswar Anand <129603787+anandchikka@users.noreply.github.com>
2024-03-15 15:07:53 +0530

    Update BeautifulMatrix.java

--- Day1/BeautifulMatrix.java
+++ Day1/BeautifulMatrix.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
-public class BeautifulMatrix
-{
+public class BeautifulMatrix{
 	public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int i1=0;
