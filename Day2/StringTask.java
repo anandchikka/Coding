5463e5963d9e19624bcbf3ec9b11113227467963
Chikka Nikhil Eswar Anand <129603787+anandchikka@users.noreply.github.com>
2024-03-17 12:10:18 +0530

    Create StringTask.java

---
+++ Day2/StringTask.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class StringTask{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		
+		str=str.toLowerCase();
+		
+		String[] vowels={"A", "O", "Y", "E", "U", "I", "a", "o", "y", "e", "u", "i"};
+		
+		for(int i=0; i<vowels.length; i++){
+		    str=str.replace(vowels[i], "");
+		}
+		
+		StringBuilder stringBuilder=new StringBuilder();
+		for(int i=0; i<str.length(); i++){
+		    stringBuilder.append(".");
+		    stringBuilder.append(str.charAt(i));
+		}
+		
+		System.out.println(stringBuilder);
+	}
+}
